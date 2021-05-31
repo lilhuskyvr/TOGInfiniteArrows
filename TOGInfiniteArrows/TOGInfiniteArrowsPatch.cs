5889bf265c0975c59b8dc95b68edadc9ef60d6c2
Lil Husky VR <lilhuskyvr@gmail.com>
2021-06-01 06:16:56 +1000

    Version 1.0

---
+++ TOGInfiniteArrows/TOGInfiniteArrowsPatch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+namespace TOGInfiniteArrows
+{
+    public class TOGInfiniteArrowsPatch : MonoBehaviour
+    {
+        private Harmony _harmony;
+
+        public void Inject()
+        {
+            try
+            {
+                _harmony = new Harmony("InfiniteArrows");
+                _harmony.PatchAll(Assembly.GetExecutingAssembly());
+                Debug.Log("Infinite Arrows Loaded");
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex.Message);
+            }
+        }
+
+
+        [HarmonyPatch(typeof(QuiverArrow))]
+        [HarmonyPatch("OnParenting")]
+        // ReSharper disable once UnusedType.Local
+        private static class QuiverArrowOnParentingPatch
+        {
+            [HarmonyPostfix]
+            private static void Postfix(QuiverArrow __instance, Stats s)
+            {
+                __instance.count = Int32.MaxValue;
+            }
+        }
+
+        [HarmonyPatch(typeof(QuiverBolt))]
+        [HarmonyPatch("OnParenting")]
+        // ReSharper disable once UnusedType.Local
+        private static class QuiverBoltOnParentingPatch
+        {
+            [HarmonyPostfix]
+            private static void Postfix(QuiverBolt __instance, Stats s)
+            {
+                BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
+                                         | BindingFlags.Static;
+                __instance.GetType().GetField("count", bindFlags).SetValue(__instance, Int32.MaxValue);
+            }
+        }
+    }
+}
